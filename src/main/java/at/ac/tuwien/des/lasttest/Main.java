933c202c48ac469f552a890b21f213c12610555a
rolandjoebstl@gmx.at <Roland>
2012-06-17 15:41:12 +0200

    Lasttest

---
+++ src/main/java/at/ac/tuwien/des/lasttest/Main.java
@@ -0,0 +1,28 @@
+package at.ac.tuwien.des.lasttest;
+
+import com.clarkware.junitperf.LoadTest;
+import com.clarkware.junitperf.TimedTest;
+
+import junit.framework.Test;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		junit.textui.TestRunner.run(startTest());
+	}
+	
+	public static Test startTest()
+	{
+		int userCount = 100;
+		long maxTimeInMillis = 2000;
+		Test test = new PublicUserTest("ListAllHospitalsTestCase");
+		Test timedTest = new TimedTest(test, maxTimeInMillis);
+		Test loadTest = new LoadTest(timedTest, userCount);
+		return loadTest;
+	}
+
+}
